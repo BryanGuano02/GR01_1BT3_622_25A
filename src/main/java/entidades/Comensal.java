2d1a4b6242c85f2845d231f9dc5248a4faf59b77
Bryan Guano <94011647+BryanGuano02@users.noreply.github.com>
2025-04-26 19:00:14 -0500

    feat: add Comensal database mapping logic

---
+++ src/main/java/entidades/Comensal.java
@@ -0,0 +1,70 @@
+package entidades;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Comensal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String correo;
+    @OneToMany
+    @JoinColumn(name = "idComensal")
+    private List<Preferencia> preferencias;
+    @OneToMany(mappedBy = "comensal")
+    private List<Calificacion> calificaciones;
+
+    public Comensal() {
+    }
+
+    public Comensal(Long id, String nombre, String correo, List<Preferencia> preferencias, List<Calificacion> calificaciones) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.preferencias = preferencias;
+        this.calificaciones = calificaciones;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public List<Preferencia> getPreferencias() {
+        return preferencias;
+    }
+
+    public void setPreferencias(List<Preferencia> preferencias) {
+        this.preferencias = preferencias;
+    }
+
+    public List<Calificacion> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(List<Calificacion> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
+}
