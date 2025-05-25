f91a01987847c3b086edcb9194b15dfc831b951a
Bryan Guano <bryanguano@epn.edu.ec>
2025-05-25 15:48:43 -0500

    Adds voting functionality for qualifications
    Implements the core logic for users to vote on qualifications.
    Includes the service for handling votes and test cases to ensure
    the voting mechanism functions as expected. Addresses the core
    functionality of the 'i1/calificacion' branch by providing the
    ability to register and retract votes.

---
+++ src/test/java/servicios/VotacionCalificacionTest.java
@@ -0,0 +1,45 @@
+package servicios;
+
+import entidades.VotoCalificacion;
+import org.junit.Test;
+
+import entidades.Calificacion;
+import entidades.Comensal;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class VotacionCalificacionServiceTest {
+
+    @Test
+    public void givenComensalCalificacion_whenVotar_thenVotoRegistradoConExito() {
+        Long idComensal = 1L;
+        Long idCalificacion = 1L;
+        Comensal comensal = new Comensal();
+        Calificacion calificacion = new Calificacion();
+        comensal.setId(idComensal);
+        calificacion.setId(idCalificacion);
+
+        VotacionService votacionService = new VotacionService();
+        votacionService.votarCalificacion(comensal, calificacion);
+
+        assertTrue(calificacion.getVotos().get(0).getComensal().getId().equals(idComensal));
+    }
+
+    @Test
+    public void givenSameComensalSameCalificacion_whenVotar_thenDeleteVotoRegistrado() {
+        Long idComensal = 1L;
+        Long idCalificacion = 1L;
+        Comensal comensal = new Comensal();
+        Calificacion calificacion = new Calificacion();
+        comensal.setId(idComensal);
+        calificacion.setId(idCalificacion);
+
+        VotacionService votacionService = new VotacionService();
+        Boolean resultadoPrimerVoto = votacionService.votarCalificacion(comensal, calificacion);
+        Boolean resultadoSegundoVoto = votacionService.votarCalificacion(comensal, calificacion);
+
+        assertTrue(resultadoPrimerVoto);
+        assertFalse(resultadoSegundoVoto);
+    }
+}
